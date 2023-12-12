56de444d8a28ba07526fe9459d43c99da836e276
Omar Aljohani <omerrr350@hotmail.com>
2023-12-12 03:03:29 +0300

    Added indication when now item added and the list is empty. Added a functionabilty to remove a list item.

--- lib/screens/groceries_screen.dart
+++ lib/screens/groceries_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/data/dummy_items.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/screens/new_item_screen.dart';
 
@@ -19,35 +18,79 @@ class _GroceriesScreenState extends State<GroceriesScreen> {
     if (newItem == null) {
       return;
     } else {
-      _groceriesList.add(newItem);
+      setState(() {
+        _groceriesList.add(newItem);
+      });
     }
+  }
+
+  void _removeItem(item) {
+    setState(() {
+      _groceriesList.remove(item);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Your Groceries"),
-        actions: [
-          IconButton(
-              onPressed: () {
-                _createItem();
-              },
-              icon: const Icon(Icons.add))
-        ],
-      ),
-      body: ListView.builder(
-        itemCount: _groceriesList.length,
-        itemBuilder: ((context, index) => ListTile(
-              title: Text(_groceriesList[index].name),
-              leading: Container(
-                width: 24,
-                height: 24,
-                color: _groceriesList[index].category.color,
-              ),
-              trailing: Text(_groceriesList[index].quantity.toString()),
-            )),
+    Widget content = Padding(
+      padding: const EdgeInsets.all(24),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Let's go for a shopping",
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Try to add new items using the + icon on the top-right corner",
+              style: TextStyle(
+                  fontSize: 16,
+                  color: Theme.of(context).colorScheme.onBackground),
+            )
+          ],
+        ),
       ),
     );
+
+    if (_groceriesList.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceriesList.length,
+        itemBuilder: ((context, index) => Dismissible(
+              onDismissed: (direction) {
+                _removeItem(_groceriesList[index]);
+              },
+              key: ValueKey(_groceriesList[index].id),
+              child: ListTile(
+                title: Text(_groceriesList[index].name),
+                leading: Container(
+                  width: 24,
+                  height: 24,
+                  color: _groceriesList[index].category.color,
+                ),
+                trailing: Text(_groceriesList[index].quantity.toString()),
+              ),
+            )),
+      );
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Your Groceries"),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  _createItem();
+                },
+                icon: const Icon(Icons.add))
+          ],
+        ),
+        body: content);
   }
 }
